b51895a293a3564295a337e2b4a697a20807bb46
Raman Pitselmakhau <ramanpitselmakhaupl@gmail.com>
2022-06-21 21:02:20 +0300

    add task3

---
+++ Task3/variables.tf
@@ -0,0 +1,30 @@
+variable "us-east-1" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    description = " all subnets"
+    default = "10.20.0.0/16"
+}
+
+variable "subnets_cidr" {
+	type = list
+	default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "subnets_private_cidr" {
+	type = list
+	default = ["10.20.0.0/24"]
+}
+
+variable "azs" {
+	type = list
+	default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "azs_private" {
+	type = list
+	default = ["us-east-1a"]
+}
+
+    
