3208b4f645c389c27d689f4c56077bdffe798257
Raman Pitselmakhau <ramanpitselmakhaupl@gmail.com>
2022-06-28 15:13:22 +0300

    task3 fixed

--- Task3/userdata_ubuntu.sh
+++ Task3/userdata_ubuntu.sh
@@ -91,7 +91,7 @@ apache2ctl configtest
 systemctl reload apache2  
 
 apt-get update
-apt-get install \
+apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -102,6 +102,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 reboot
