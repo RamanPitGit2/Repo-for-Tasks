4fcc8939c5e132f7e2f3d66ecab4ce2214fdb377
Raman Pitselmakhau <ramanpitselmakhaupl@gmail.com>
2022-07-01 19:36:06 +0300

    task5 main

--- Task5/playbook/roles/docker/files/docker_ubuntu.sh
+++ Task5/playbook/roles/docker/files/docker_ubuntu.sh
@@ -6,7 +6,7 @@ apt-get install -y \
     lsb-release
 mkdir -p /etc/apt/keyrings
 rm -f /etc/apt/keyrings/docker.gpg
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg  --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
---
+++ Task5/terraform/security.tf
@@ -0,0 +1,50 @@
+# Create the Public Security Group
+resource "aws_security_group" "Security_Group_Public" {
+  vpc_id       = aws_vpc.terra_vpc.id
+  name         = "public"
+  description  = "Security Group Public"
+  
+  # allow ingress of port 22 for tcp
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  # allow ingress of port 80 for http
+  ingress { 
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+ # allow ingress of port 433 for https
+  ingress { 
+    from_port   = 433
+    to_port     = 433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  # allow ingress of icmp 
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+  
+  # allow egress of all ports
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" #tcp
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+tags = {
+   Name = "Security Group Public"
+   Description = "Security Group Public"
+}
+}
